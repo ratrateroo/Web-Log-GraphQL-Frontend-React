a636e55e81d38013bb72a4a5487b827b82adc6e4
Mark <ratrateroo@gmail.com>
2020-05-01 00:35:34 +0800

    added switch component

--- web-log/src/App.js
+++ web-log/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 
 import Users from  './user/pages/Users';
+import NewBlog from './blogs/pages/NewBlog';
 const App = () => {
   return <Router>
+    <Switch>
     <Route path="/" exact>
       <Users />
     </Route>
+    <Route path="/blogs/new" exact>
+      <NewBlog />
+    </Route>
     <Redirect to="/" />
+    </Switch>
   </Router>
 }
 
