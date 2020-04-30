b83a3f81619d5b619eb6d1401196829e420f5947
Mark <ratrateroo@gmail.com>
2020-05-01 00:30:28 +0800

    stated developing the app

--- web-log/src/App.js
+++ web-log/src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Users from  './user/pages/Users';
+const App = () => {
+  return <Router>
+    <Route path="/" exact>
+      <Users />
+    </Route>
+    <Redirect to="/" />
+  </Router>
 }
 
 export default App;
