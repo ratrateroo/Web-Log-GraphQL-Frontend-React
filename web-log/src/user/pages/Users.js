b52fe88a377a27f30bddefe4e3a3a28ba075d821
Mark <ratrateroo@gmail.com>
2020-05-01 23:39:23 +0800

    added users components

--- web-log/src/user/pages/Users.js
+++ web-log/src/user/pages/Users.js
@@ -1,7 +1,12 @@
 import React from ' react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-   return <h2>Users Works</h2>
+
+   const USERS = [{id: 'u1', name: 'Aurora Barnuts', image: 'https://cdn-img.instyle.com/sites/default/files/styles/640x768/public/1571415335/jennifer%20lawrence%20wedding%20dress%20lead.jpg', blogs: 3}];
+
+   return <UsersList items={USERS}/>
 };
 
 export default Users;
