9316f3f623728b7f180bfe3b6bb232a7908591e4
Mark <ratrateroo@gmail.com>
2020-05-12 23:05:14 +0800

    started fixing the blog list

--- web-log/src/blogs/components/BlogList.js
+++ web-log/src/blogs/components/BlogList.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import Card from '../../shared/components/UIElements/Card';
 import './BlogList.css'
 
 const BlogList = props => {
-    return
+    if (props.items.length === 0) {
+        return <div className="blog-list center">
+            <Card>
+                <h2>No Blogs Found.</h2>
+                <button>Share a Blog</button>
+            </Card>
+        </div>
+    }
 };
 
 export default BlogList;
