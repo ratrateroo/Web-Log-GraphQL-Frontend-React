3ff39ca5cd3cb4c08365de9d61334bbbc4125c08
Mark <ratrateroo@gmail.com>
2020-05-12 22:58:32 +0800

    added blog item

---
+++ web-log/src/blogs/components/BlogItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './BlogItem.css';
+
+const BlogItem = props => {
+    return
+};
+
+export default BlogItem;
---
+++ web-log/src/blogs/components/BlogList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './BlogList.css'
+
+const BlogList = props => {
+    return
+};
+
+export default BlogList;
