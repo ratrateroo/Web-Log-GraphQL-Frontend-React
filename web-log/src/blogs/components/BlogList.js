4d45e61f7de641f398bf1749f483807fb66aac71
Mark <ratrateroo@gmail.com>
2020-05-13 22:24:03 +0800

    added blog items to the blog list

--- web-log/src/blogs/components/BlogList.js
+++ web-log/src/blogs/components/BlogList.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
+import BlogItem from './BlogItem';
 import './BlogList.css'
+
 
 const BlogList = props => {
     if (props.items.length === 0) {
-        return <div className="blog-list center">
+        return (
+        <div className="blog-list center">
             <Card>
                 <h2>No Blogs Found.</h2>
                 <button>Share a Blog</button>
             </Card>
         </div>
+        );
     }
+
+    return <ul className="blog-list">
+        {props.items.map(blog => <BlogItem key={blog.id} id={blog.id} image={blog.imageUrl} title={blog.title} creatorId={blog.creator}/>)}
+    </ul>
 };
 
 export default BlogList;
