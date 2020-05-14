78f37b06cea2b885f1498560211abbc4171a91be
Mark <ratrateroo@gmail.com>
2020-05-14 23:02:27 +0800

    added blog item component

--- web-log/src/blogs/components/BlogItem.js
+++ web-log/src/blogs/components/BlogItem.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import Card from '../../shared/components/UIElements/Card';
 import './BlogItem.css';
 
 const BlogItem = props => {
-    return
+    return <li className="blog-item">
+        <Card className="blog-item__content">
+        <div className="blog-item__image">
+            <img src={props.image} alt={props.title} />
+        </div>
+        <div className="blog-item__info">
+            <h2>{props.title}</h2>
+<h3>{props.address}</h3>
+        <p>{props.description}</p>
+        </div>
+        <div className="blog-item__actions">
+            <button>View</button>
+            <button>Edit</button>
+            <button>Delete</button>
+        </div>
+        </Card>
+    </li>
 };
 
 export default BlogItem;
