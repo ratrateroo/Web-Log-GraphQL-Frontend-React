2187540b4790f7c8c962b5c4f489c1e875d7cec0
Mark <ratrateroo@gmail.com>
2020-05-16 23:42:34 +0800

    updated user blogs

--- web-log/src/blogs/pages/UserBlogs.js
+++ web-log/src/blogs/pages/UserBlogs.js
@@ -1,7 +1,18 @@
 import react from 'react';
 
+import BlogList from '../components/BlogList';
+
+const DUMMY_BLOGS = [
+    {id: 'b1',
+    title: 'my title',
+    description: 'my description',
+    imageUrl: 'my-image-url',
+    creator: 'Aurora Barnuts'
+    }
+];
+
 const UserBlogs = () => {
-    return
+    return <BlogList items={DUMMY_BLOGS}/>
 };
 
 export default UserBlogs
