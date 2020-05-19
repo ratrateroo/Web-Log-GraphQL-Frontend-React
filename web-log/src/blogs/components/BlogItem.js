b349e29ab51ced752909026e1ab900dc826e2ce3
Mark <ratrateroo@gmail.com>
2020-05-19 23:28:04 +0800

    added modal to the blog item

--- web-log/src/blogs/components/BlogItem.js
+++ web-log/src/blogs/components/BlogItem.js
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
+import Modal from '../../shared/components/UIElements/Modal';
 import './BlogItem.css';
 
 const BlogItem = props => {
-    return <li className="blog-item">
-        <Card className="blog-item__content">
-        <div className="blog-item__image">
-            <img src={props.image} alt={props.title} />
-        </div>
-        <div className="blog-item__info">
-            <h2>{props.title}</h2>
-<h3>{props.address}</h3>
-        <p>{props.description}</p>
-        </div>
-        <div className="blog-item__actions">
-            <Button inverse>View</Button>
-            <Button to={`/blogs/${props.id}`}>Edit</Button>
-            <Button danger>Delete</Button>
-        </div>
-        </Card>
-    </li>
+    const [showMap, setShowMap] = useState(false);
+
+    const opemMapHandler = () => setShowMap(true);
+
+    const closeMapHandler = () => setShowMap(false);
+
+    return 
+    <React.Fragment>
+        <Modal show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass="blog-item__modal-content"
+        footerClass="blog-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>Close</Button>}/>
+        <li className="blog-item">
+            <Card className="blog-item__content">
+                <div className="blog-item__image">
+                    <img src={props.image} alt={props.title} />
+                </div>
+                <div className="blog-item__info">
+                    <h2>{props.title}</h2>
+                    <h3>{props.address}</h3>
+                    <p>{props.description}</p>
+                </div>
+                <div className="blog-item__actions">
+                    <Button inverse>View</Button>
+                    <Button to={`/blogs/${props.id}`}>Edit</Button>
+                    <Button danger>Delete</Button>
+                </div>
+            </Card>
+        </li>
+    </React.Fragment>
 };
 
 export default BlogItem;
