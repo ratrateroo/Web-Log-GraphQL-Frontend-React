cf0e42a4954ba31786e1b883fda15e5bf9491923
Mark <ratrateroo@gmail.com>
2020-05-30 22:52:48 +0800

    added validator to the input and new blog

--- web-log/src/blogs/pages/NewBlog.js
+++ web-log/src/blogs/pages/NewBlog.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators'
 import './NewBlog.css';
 
 const NewBlog = () => {
     return <form className="blog-form">
-        <Input type="text" label="Title" validators={[]} errorText="Please enter a valid title"/>
+        <Input type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title"/>
     </form>
 };
 
