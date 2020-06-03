e241294e67f387f205245c5b0583624c6811812a
Mark <ratrateroo@gmail.com>
2020-06-03 20:55:50 +0800

    managed form wide state

--- web-log/src/blogs/pages/NewBlog.js
+++ web-log/src/blogs/pages/NewBlog.js
@@ -1,12 +1,74 @@
-import React from 'react';
+import React , { useCallback, useReducer } from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
-import { VALIDATOR_REQUIRE } from '../../shared/util/validators'
+import Button from '../../shared/components/FormElements/Button';
+import { VALIDATOR_REQUIRE, VALIDATOR_MAXLENGTH } from '../../shared/util/validators'
 import './NewBlog.css';
 
+const formReducer = (state, action) => {
+    switch (action.type) {
+        case 'INPUT_CHANGE':
+        let formIsValid = true;
+        for(const inputId in state.inputs) {
+            if (inputId === action.inputId) {
+                formIsValid = formIsValid && action.isValid;
+            } else {
+                formIsValid = formIsValid && state.inputs[inputId].isValid;
+            }
+        }
+        return {
+            ...state,
+            inputs: {
+                ...state.inputs,
+                [action.inputId]: { value: action.value, isValid: action.isValid }
+            },
+            isValid: formIsValid
+        };
+    default:
+        return state;
+
+    }
+};
+
 const NewBlog = () => {
+    const [formState, dispatch] = useReducer(formReducer, {
+        inputs: {
+            title: {
+                value: '',
+                isValid: false
+            },
+            description: {
+                value: '',
+                isValid: false
+            }
+        },
+        isValid: false
+    });
+    const inputHandler = useCallback((id, value, isValid) => {
+        dispatch({type: 'INPUT_CHANGE', value: value, isValid: isValid, inputId: id})
+    }, []);
+
+
     return <form className="blog-form">
-        <Input type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title"/>
+        <Input
+        id="title"
+        element="input"
+        type="text" 
+        label="Title" 
+        validators={[VALIDATOR_REQUIRE()]} 
+        errorText="Please enter a valid title"
+        onInput = {inputHandler}
+        />
+
+<Input 
+        id="description"
+        element="textarea"
+        label="Description" 
+        validators={[VALIDATOR_MAXLENGTH(5)]} 
+        errorText="Please enter a valid description (at least 5 characters)."
+        onInput = {inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>Add Blog</Button>
     </form>
 };
 
