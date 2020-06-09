b1c9c13de6d9c336e99ce4cf729a4466533a95c5
Mark <ratrateroo@gmail.com>
2020-06-09 23:28:33 +0800

    started using custom hooks

--- web-log/src/blogs/pages/NewBlog.js
+++ web-log/src/blogs/pages/NewBlog.js
@@ -5,30 +5,7 @@ import Button from '../../shared/components/FormElements/Button';
 import { VALIDATOR_REQUIRE, VALIDATOR_MAXLENGTH } from '../../shared/util/validators'
 import './BlogForm.css';
 
-const formReducer = (state, action) => {
-    switch (action.type) {
-        case 'INPUT_CHANGE':
-        let formIsValid = true;
-        for(const inputId in state.inputs) {
-            if (inputId === action.inputId) {
-                formIsValid = formIsValid && action.isValid;
-            } else {
-                formIsValid = formIsValid && state.inputs[inputId].isValid;
-            }
-        }
-        return {
-            ...state,
-            inputs: {
-                ...state.inputs,
-                [action.inputId]: { value: action.value, isValid: action.isValid }
-            },
-            isValid: formIsValid
-        };
-    default:
-        return state;
 
-    }
-};
 
 const NewBlog = () => {
     const [formState, dispatch] = useReducer(formReducer, {
