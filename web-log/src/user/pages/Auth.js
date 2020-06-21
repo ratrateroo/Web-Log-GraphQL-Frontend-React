141680735751582d683c6aa7744944de22b7accc
Mark <ratrateroo@gmail.com>
2020-06-21 23:33:39 +0800

    added more inputs

--- web-log/src/user/pages/Auth.js
+++ web-log/src/user/pages/Auth.js
@@ -8,7 +8,7 @@ import { useForm } from '../../shared/hooks/form-hook';
 import './Auth.css';
 
 const Auth = () => {
-    useForm({
+    const [formState, inputHandler] = useForm({
         email: {
             value: '',
             isValid: false
@@ -18,14 +18,32 @@ const Auth = () => {
         }
     });
 
+    const authSubmitHandler = event => {
+        event.preventDefault();
+        console.log(formState.inputs)
+    }
+
     return <Card>
         <h2>Login Required</h2>
         <hr />
-        <form>
-            <Input element="input" id="email" type="email" label="E-Mail" 
+        <form onSubmit={authSubmitHandler}>
+            <Input 
+            element="input" 
+            id="email" 
+            type="email" 
+            label="E-Mail" 
             validators={[VALIDATOR_EMAIL()]}
-            errorText="Please enter a valid email address."/>
-            <Button></Button>
+            errorText="Please enter a valid email address."
+            onInput={inputHandler} />
+             <Input 
+            element="input" 
+            id="password" 
+            type="password" 
+            label="Password" 
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid password."
+            onInput={inputHandler} />
+            <Button type="submit" disabled={!formState.isValid}>LOGIN</Button>
         </form>
     </Card>
 };
