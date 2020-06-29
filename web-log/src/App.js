398a8858d018845209450e1fa2b4df0ea03f12c7
Mark <ratrateroo@gmail.com>
2020-06-29 20:53:48 +0800

    updated routes for authenticated user

--- web-log/src/App.js
+++ web-log/src/App.js
@@ -20,26 +20,45 @@ const App = () => {
     setIsLoggedIn(false)
   }, []);
 
-  return (
-  <AuthContext.Provider value={{isLoggedIn: isLoggedIn, login: login, logout: logout }}>
-    <Router>
-    <MainNavigation/>
-      <Switch>
-        <Route path="/" exact>
+   let routes;
+
+    if (isLoggedIn) {
+        routes = ();
+    } else {
+        
+        routes = (
+            //not logged in routes
+            //root route
+            //blogs route
+            //auth route
+            <React.Fragment>
+<Route path="/" exact>
           <Users />
         </Route>
         <Route path="/:userId/blogs" exact>
           <UserBlogs />
         </Route>
+        <Route path="/auth">
+          <Auth/>
+        </Route>
+        <Redirect to="/auth" />
+            </React.Fragment>
+        );
+    }
+
+  return (
+  <AuthContext.Provider value={{isLoggedIn: isLoggedIn, login: login, logout: logout }}>
+    <Router>
+    <MainNavigation/>
+      <Switch>
+        
         <Route path="/blogs/new" exact>
           <NewBlog />
         </Route>
         <Route path="/blogs/:blogId">
           <UpdateBlog/>
         </Route>
-        <Route path="/auth">
-          <Auth/>
-        </Route>
+        
         <Redirect to="/" />
       </Switch>
   </Router>
--- web-log/src/user/pages/Auth.js
+++ web-log/src/user/pages/Auth.js
@@ -1,13 +1,16 @@
-import React ,{ useState } from 'react';
+import React ,{ useState, useContext } from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
+import { AuthContext } from '../../shared/context/auth-context';
 import './Auth.css';
 
 const Auth = () => {
+
+    const auth = useContext(AuthContext);
 
     const [isLoginMode, setIsLoginMode] = useState(true);
     const [formState, inputHandler, setFormData] = useForm({
@@ -42,8 +45,11 @@ const Auth = () => {
 
     const authSubmitHandler = event => {
         event.preventDefault();
-        console.log(formState.inputs)
+        console.log(formState.inputs);
+        auth.login();
     }
+
+   
 
     return <Card>
         <h2>Login Required</h2>
