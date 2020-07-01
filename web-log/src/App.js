d60844d83ad26aa835cfa3e2bd10e97a8e1ab438
Mark <ratrateroo@gmail.com>
2020-07-01 21:01:43 +0800

    removed disabled js and react from disabled lang

--- web-log/src/App.js
+++ web-log/src/App.js
@@ -1,7 +1,12 @@
-import React, { useState,useCallback }from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+import React, { useState, useCallback } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
-import Users from  './user/pages/Users';
+import Users from './user/pages/Users';
 import NewBlog from './blogs/pages/NewBlog';
 import UserBlogs from './blogs/pages/UserBlogs';
 import UpdateBlog from './blogs/pages/UpdateBlog';
@@ -10,22 +15,21 @@ import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
 
-
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const login = useCallback(() => {
-    setIsLoggedIn(true) 
+    setIsLoggedIn(true);
   }, []);
 
   const logout = useCallback(() => {
-    setIsLoggedIn(false) 
+    setIsLoggedIn(false);
   }, []);
 
-   let routes;
+  let routes;
 
-    if (isLoggedIn) {
-        routes = (
-        <React.Fragment>
+  if (isLoggedIn) {
+    routes = (
+      <React.Fragment>
         <Route path="/" exact>
           <Users />
         </Route>
@@ -33,52 +37,51 @@ const App = () => {
           <UserBlogs />
         </Route>
         <Route path="/auth">
-          <Auth/>
+          <Auth />
         </Route>
         <Redirect to="/auth" />
-            </React.Fragment>
-        );
-    } else {
-        
-        routes = (
-            //not logged in routes
-            //root route
-            //blogs route
-            //auth route
-            <React.Fragment>
-<Route path="/" exact>
+      </React.Fragment>
+    );
+  } else {
+    routes = (
+      //not logged in routes
+      //root route
+      //blogs route
+      //auth route
+      <React.Fragment>
+        <Route path="/" exact>
           <Users />
         </Route>
         <Route path="/:userId/blogs" exact>
           <UserBlogs />
         </Route>
         <Route path="/auth">
-          <Auth/>
+          <Auth />
         </Route>
         <Redirect to="/auth" />
-            </React.Fragment>
-        );
-    }
+      </React.Fragment>
+    );
+  }
 
   return (
-  <AuthContext.Provider value={{isLoggedIn: isLoggedIn, login: login, logout: logout }}>
-    <Router>
-    <MainNavigation/>
-      <Switch>
-        
-        <Route path="/blogs/new" exact>
-          <NewBlog />
-        </Route>
-        <Route path="/blogs/:blogId">
-          <UpdateBlog/>
-        </Route>
-        
-        <Redirect to="/" />
-      </Switch>
-  </Router>
-  </AuthContext.Provider>
-  
-  )
-}
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <Switch>
+          <Route path="/blogs/new" exact>
+            <NewBlog />
+          </Route>
+          <Route path="/blogs/:blogId">
+            <UpdateBlog />
+          </Route>
+
+          <Redirect to="/" />
+        </Switch>
+      </Router>
+    </AuthContext.Provider>
+  );
+};
 
 export default App;
