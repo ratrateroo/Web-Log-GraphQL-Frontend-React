aaa9598ba9363e1f26a74dac6089027f73812764
Mark <ratrateroo@gmail.com>
2020-07-02 11:06:08 +0800

    added logout button

--- web-log/src/blogs/components/BlogItem.js
+++ web-log/src/blogs/components/BlogItem.js
@@ -1,75 +1,91 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
 import Modal from '../../shared/components/UIElements/Modal';
+import { AuthContext } from '../../shared/context/auth-context';
 import './BlogItem.css';
 
-const BlogItem = props => {
-    const [showMap, setShowMap] = useState(false);
-    const [showConfirmModal, setShowConfirmModal] = useState(false);
+const BlogItem = (props) => {
+   const auth = useContext(AuthContext);
+   const [showMap, setShowMap] = useState(false);
+   const [showConfirmModal, setShowConfirmModal] = useState(false);
 
-    const opemMapHandler = () => setShowMap(true);
+   const opemMapHandler = () => setShowMap(true);
 
-    const closeMapHandler = () => setShowMap(false);
+   const closeMapHandler = () => setShowMap(false);
 
-    const showDeleteWarningHandler = () => {
-        setShowConfirmModal(true);
-    };
+   const showDeleteWarningHandler = () => {
+      setShowConfirmModal(true);
+   };
 
-    const cancelDeleteHandler = () => {
-        setShowConfirmModal(false);
-    };
+   const cancelDeleteHandler = () => {
+      setShowConfirmModal(false);
+   };
 
-    const confirmDeleteHandler = () => {
-        setShowConfirmModal(false);
-    };
-    
+   const confirmDeleteHandler = () => {
+      setShowConfirmModal(false);
+   };
 
-    return (
-    <React.Fragment>
-        <Modal show={showMap}
-        onCancel={closeMapHandler}
-        header={props.address}
-        contentClass="blog-item__modal-content"
-        footerClass="blog-item__modal-actions"
-        footer={<Button onClick={closeMapHandler}>Close</Button>}>
+   return (
+      <React.Fragment>
+         <Modal
+            show={showMap}
+            onCancel={closeMapHandler}
+            header={props.address}
+            contentClass="blog-item__modal-content"
+            footerClass="blog-item__modal-actions"
+            footer={<Button onClick={closeMapHandler}>Close</Button>}
+         >
             <div className="map-container">
-                <h2>The Map</h2>
+               <h2>The Map</h2>
             </div>
-        </Modal>
-        <Modal 
-        show={showConfirmModal}
-        onCancel={cancelDeleteHandler}
-        header="Are you sure?" 
-        footerClass="blog-item__modal-actions" 
-        footer={
-            <React.Fragment>
-                <Button inverse onCancel={cancelDeleteHandler}>CANCEL</Button>
-                <Button danger onCancel={confirmDeleteHandler}>DELETE</Button>
-            </React.Fragment>
-        }>
+         </Modal>
+         <Modal
+            show={showConfirmModal}
+            onCancel={cancelDeleteHandler}
+            header="Are you sure?"
+            footerClass="blog-item__modal-actions"
+            footer={
+               <React.Fragment>
+                  <Button inverse onCancel={cancelDeleteHandler}>
+                     CANCEL
+                  </Button>
+                  <Button danger onCancel={confirmDeleteHandler}>
+                     DELETE
+                  </Button>
+               </React.Fragment>
+            }
+         >
             <p>Do you want to proceed and delete this blog?</p>
-        </Modal>
-        <li className="blog-item">
+         </Modal>
+         <li className="blog-item">
             <Card className="blog-item__content">
-                <div className="blog-item__image">
-                    <img src={props.image} alt={props.title} />
-                </div>
-                <div className="blog-item__info">
-                    <h2>{props.title}</h2>
-                    <h3>{props.address}</h3>
-                    <p>{props.description}</p>
-                </div>
-                <div className="blog-item__actions">
-                    <Button inverse onClick={opemMapHandler}>View</Button>
-                    <Button to={`/blogs/${props.id}`}>Edit</Button>
-                    <Button danger onCancel={showDeleteWarningHandler}>Delete</Button>
-                </div>
+               <div className="blog-item__image">
+                  <img src={props.image} alt={props.title} />
+               </div>
+               <div className="blog-item__info">
+                  <h2>{props.title}</h2>
+                  <h3>{props.address}</h3>
+                  <p>{props.description}</p>
+               </div>
+               <div className="blog-item__actions">
+                  <Button inverse onClick={opemMapHandler}>
+                     View
+                  </Button>
+                  {auth.isLoggedIn && (
+                     <Button to={`/blogs/${props.id}`}>Edit</Button>
+                  )}
+                  {auth.isLoggedIn && (
+                     <Button danger onCancel={showDeleteWarningHandler}>
+                        Delete
+                     </Button>
+                  )}
+               </div>
             </Card>
-        </li>
-    </React.Fragment>
-    );
+         </li>
+      </React.Fragment>
+   );
 };
 
 export default BlogItem;
--- web-log/src/shared/components/Navigation/NavLinks.js
+++ web-log/src/shared/components/Navigation/NavLinks.js
@@ -1,45 +1,91 @@
-import React, { useContext }from 'react';
-import { NavLink } from 'react-router-dom'
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
 
 import { AuthContext } from '../../context/auth-context';
 import './NavLinks.css';
 
-const NavLinks = props => {
-    const auth = useContext(AuthContext);
-    return (  
-        
-       
-        <ul className="nav-links">
+const NavLinks = (props) => {
+   const auth = useContext(AuthContext);
+   return (
+      <ul className="nav-links">
+         <li>
+            <NavLink to="/" exact>
+               All Users
+            </NavLink>
+         </li>
+         {auth.isLoggedIn && (
             <li>
-                <NavLink to="/" exact>All Users</NavLink>
+               <NavLink to="/u1/blogs" exact>
+                  My Blogs
+               </NavLink>
             </li>
-            {auth.isLoggedIn && (<li>
-                <NavLink to="/u1/blogs" exact>My Blogs</NavLink>
-            </li>)}
-            <li className="nav-link">
-                <a href="../index.html" className="nav-link-item active">Link</a>
-            </li>
-            <li className="nav-link">
-                <a href="../Blog List/Blog List.html" className="nav-link-item">Blog
-                    List</a>
-            </li>
-            <li className="nav-link"><a href="../Blog/Blog.html" className="nav-link-item">Blog</a></li>
-            <li className="nav-link"><a href="../Sidebar/Sidebar.html" className="nav-link-item">Sidebar</a></li>
-            <li className="nav-link"><a href="../Signup/Signup.html" className="nav-link-item">Sign Up</a></li>
-            <li className="nav-link"><a href="../Login/Login.html" className="nav-link-item">Log In</a></li>
-            <li className="nav-link"><a href="../Logo/Logo.html" className="nav-link-item">Logo</a></li>
-            <li className="nav-link"><a href="../Navigiation/Navigation.html" className="nav-link-item">Navigation</a>
-            </li>
-            <li className="nav-link"><a href="../Header/Header.html" className="nav-link-item">Header</a></li>
-            <li className="nav-link"><a href="../Footer/Footer.html" className="nav-link-item">Footer</a></li>
-            <li className="nav-link"><a href="../User Profile/User Profile.html" className="nav-link-item">User
-                    Profile</a>
-            </li>
-        </ul>
-    
-    
-     );
+         )}
+         <li className="nav-link">
+            <a href="../index.html" className="nav-link-item active">
+               Link
+            </a>
+         </li>
+         <li className="nav-link">
+            <a href="../Blog List/Blog List.html" className="nav-link-item">
+               Blog List
+            </a>
+         </li>
+         <li className="nav-link">
+            <a href="../Blog/Blog.html" className="nav-link-item">
+               Blog
+            </a>
+         </li>
+         <li className="nav-link">
+            <a href="../Sidebar/Sidebar.html" className="nav-link-item">
+               Sidebar
+            </a>
+         </li>
+         <li className="nav-link">
+            <a href="../Signup/Signup.html" className="nav-link-item">
+               Sign Up
+            </a>
+         </li>
+         <li className="nav-link">
+            <a href="../Login/Login.html" className="nav-link-item">
+               Log In
+            </a>
+         </li>
+         <li className="nav-link">
+            <a href="../Logo/Logo.html" className="nav-link-item">
+               Logo
+            </a>
+         </li>
+         <li className="nav-link">
+            <a href="../Navigiation/Navigation.html" className="nav-link-item">
+               Navigation
+            </a>
+         </li>
+         <li className="nav-link">
+            <a href="../Header/Header.html" className="nav-link-item">
+               Header
+            </a>
+         </li>
+         <li className="nav-link">
+            <a href="../Footer/Footer.html" className="nav-link-item">
+               Footer
+            </a>
+         </li>
+         <li className="nav-link">
+            <a
+               href="../User Profile/User Profile.html"
+               className="nav-link-item"
+            >
+               User Profile
+            </a>
+         </li>
 
+         {auth.isLoggedIn && (
+            <li>
+               <button onClick={auth.logout}>Log Out</button>
+            </li>
+         )}
+      </ul>
+   );
 };
 
 export default NavLinks;
