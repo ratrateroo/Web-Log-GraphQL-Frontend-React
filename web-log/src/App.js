841ee1a0d7ed62cfb9eff45061b742f8cfdcf1a8
Mark <ratrateroo@gmail.com>
2020-07-02 10:47:16 +0800

    updated the auth and non auth route

--- web-log/src/App.js
+++ web-log/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useCallback } from 'react';
 import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch,
+   BrowserRouter as Router,
+   Route,
+   Redirect,
+   Switch,
 } from 'react-router-dom';
 
 import Users from './user/pages/Users';
@@ -14,74 +14,69 @@ import Auth from './user/pages/Auth';
 
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
+import { SwitchTransition } from 'react-transition-group';
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const login = useCallback(() => {
-    setIsLoggedIn(true);
-  }, []);
+   const [isLoggedIn, setIsLoggedIn] = useState(false);
+   const login = useCallback(() => {
+      setIsLoggedIn(true);
+   }, []);
 
-  const logout = useCallback(() => {
-    setIsLoggedIn(false);
-  }, []);
+   const logout = useCallback(() => {
+      setIsLoggedIn(false);
+   }, []);
 
-  let routes;
+   let routes;
 
-  if (isLoggedIn) {
-    routes = (
-      <React.Fragment>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/:userId/blogs" exact>
-          <UserBlogs />
-        </Route>
-        <Route path="/auth">
-          <Auth />
-        </Route>
-        <Redirect to="/auth" />
-      </React.Fragment>
-    );
-  } else {
-    routes = (
-      //not logged in routes
-      //root route
-      //blogs route
-      //auth route
-      <React.Fragment>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/:userId/blogs" exact>
-          <UserBlogs />
-        </Route>
-        <Route path="/auth">
-          <Auth />
-        </Route>
-        <Redirect to="/auth" />
-      </React.Fragment>
-    );
-  }
+   if (isLoggedIn) {
+      routes = (
+         <Switch>
+            <Route path="/" exact>
+               <Users />
+            </Route>
+            <Route path="/:userId/blogs" exact>
+               <UserBlogs />
+            </Route>
+            <Route path="/blogs/new" exact>
+               <NewBlog />
+            </Route>
+            <Route path="/blogs/:blogId">
+               <UpdateBlog />
+            </Route>
+            <Redirect to="/" />
+         </Switch>
+      );
+   } else {
+      routes = (
+         //not logged in routes
+         //root route
+         //blogs route
+         //auth route
+         <Switch>
+            <Route path="/" exact>
+               <Users />
+            </Route>
+            <Route path="/:userId/blogs" exact>
+               <UserBlogs />
+            </Route>
+            <Route path="/auth">
+               <Auth />
+            </Route>
+            <Redirect to="/auth" />
+         </Switch>
+      );
+   }
 
-  return (
-    <AuthContext.Provider
-      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
-    >
-      <Router>
-        <MainNavigation />
-        <Switch>
-          <Route path="/blogs/new" exact>
-            <NewBlog />
-          </Route>
-          <Route path="/blogs/:blogId">
-            <UpdateBlog />
-          </Route>
-
-          <Redirect to="/" />
-        </Switch>
-      </Router>
-    </AuthContext.Provider>
-  );
+   return (
+      <AuthContext.Provider
+         value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+      >
+         <Router>
+            <MainNavigation />
+            {routes}
+         </Router>
+      </AuthContext.Provider>
+   );
 };
 
 export default App;
